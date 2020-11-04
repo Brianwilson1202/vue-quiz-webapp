43ca2e56e660c71068f86c739ca809c4139da30e
Brianwilson1202 <brianwilson1202@gmail.com>
2020-11-04 21:49:53 +0800

    config: new repo

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <b-nav style="align-items: center" tabs>
+      <b-nav-item disabled><b>Vue Quiz Game</b></b-nav-item>
+      <b-nav-item disabled>Correct: {{ numCorrect }}</b-nav-item>
+      <b-nav-item disabled>Total: {{ numTotal }}</b-nav-item>
+      <b-nav-item>
+        <b-button @click.prevent="reloadPage" variant="success">Restart</b-button>
+      </b-nav-item>
+    </b-nav>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"],
+  methods: {
+  reloadPage(){
+    window.location.reload()
+  }
+}
+};
+</script>
